bd0d9e1f3d8bb1d53250e67aa8702512c8645a64
Dang Huy Hoang <doiquanhieumk@gmail.com>
2023-01-31 11:39:36 +0700

    hotfix

--- thefirstrails/app/controllers/application_controller.rb
+++ thefirstrails/app/controllers/application_controller.rb
@@ -5,4 +5,5 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit :sign_up, keys: [:name, :age, :avatar]
       devise_parameter_sanitizer.permit :account_update, keys: [:name, :age, :avatar]
     end
+
 end
